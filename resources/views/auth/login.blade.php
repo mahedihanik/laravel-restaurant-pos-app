c0fb9a7f195ffd7123d5c47ec156cb19260d1179
DESKTOP-BCLPK3H\Sakib N Alok <mahedi.h.anik@gmail.com>
2020-12-11 07:38:10 +0600

    bugs

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,16 +1,15 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+<div class="container mt-5">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header"><i class="fas fa-user"></i>{{ __('Login') }}</div>
+                <div class="card-header"><i class="fas fa-user"></i>{{ __('') }}</div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
-
                         <div class="form-group row">
                             <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
 
